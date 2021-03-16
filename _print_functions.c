653dbc0a2c3476734c0e644d01ba96a7eab06e2a
lemejiamo <77980644+lemejiamo@users.noreply.github.com>
2021-03-16 13:08:34 -0500

    added case print percent symbol

--- _print_functions.c
+++ _print_functions.c
@@ -8,8 +8,8 @@
 */
 int print_char(char *vc)
 {
-write(1, &vc, 1);
-return (1);
+	write(1, &vc, 1);
+	return (1);
 }
 /* end of the function*/
 
@@ -22,12 +22,12 @@ return (1);
 int print_string(char *vp)
 {
 	int bp = 0;
+
 	bp = _strlen(vp); /*bp bytes a imprimir */
 	write(1, vp, bp);
 	return (bp);
 }
 /* end of the function*/
-
 
 /* beging of funtion print_integer */
 /**
@@ -37,37 +37,40 @@ int print_string(char *vp)
 */
 int print_integer(int n)
 {
-int size, i = 0, mod = 0;
-unsigned int num;
-char numero[8], c;
+	int size, i = 0, mod = 0;
+	unsigned int num;
+	char numero[8], c;
+	char sign;
+
 	if (n < 0)
 	{
-		numero[i] = '-';
-		i++;
+		sign = '-';
+		size += 1;
+		numero[i] = sign;
 		num = -n;
+		write(1, &sign, 1);
 	}
 	else
 	{
 		num = n;
 	}
 
-for (; num > 0 ; i++)
-{
-	mod = num % 10;
-	numero[i] = (char)mod;
-	num /= 10;
-}
-size = i;
-i--;
-for (; i >= 0; i--)
-{
-	c = (numero[i] + '0');
-	write(1, &c, 1);
-}
-return (size);
+	for (; num > 0; i++)
+	{
+		mod = num % 10;
+		numero[i] = (char)mod;
+		num /= 10;
+	}
+	size += i;
+	i--;
+	for (; i >= 0; i--)
+	{
+		c = (numero[i] + '0');
+		write(1, &c, 1);
+	}
+	return (size);
 }
 /* end of the function*/
-
 
 /* beging of the function _strlen */
 
@@ -78,9 +81,23 @@ return (size);
 */
 int _strlen(char *s)
 {
-int n = 0;
-while (*s++ != '\0')
-	n++;
-return (n);
+	int n = 0;
+
+	while (*s++ != '\0')
+		n++;
+	return (n);
 }
 /* end of the function*/
+
+/**
+* print_percent - write a percent sign in stdout
+* @s: always void
+* Return: number of printed chars
+*/
+int print_percent(char *s)
+{
+	char c = '%';
+
+	write(1, &c, 1);
+	return (1);
+}
