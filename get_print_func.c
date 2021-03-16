f6521bd8d7706663d66f0087cc73c062f2c4936e
lemejiamo <77980644+lemejiamo@users.noreply.github.com>
2021-03-16 13:09:11 -0500

    added case print percent symbol

--- get_print_func.c
+++ get_print_func.c
@@ -2,44 +2,47 @@
 /**
 *get_print_int - name function to called type dates
 * @s: parameter type char
-* Return: value of op_s the pocision 
+* Return: value of op_s the pocision
 */
 int (*get_print_int(char s))(int)
 {
-    i_int list[] = {       
-        {'d', print_integer},
-        {'i', print_integer},
-        };
-    int i = 0;
-    while (list[i].op != '\0')
-    {
-        if(list[i].op == s)
-        {
-            return ((list[i]).f);
-        }
-        i++;
-    }
-    return (0);
+	i_int list[] = {
+		{'d', print_integer},
+		{'i', print_integer},
+	};
+	int i = 0;
+
+	while (list[i].op != '\0')
+	{
+		if (list[i].op == s)
+		{
+			return ((list[i]).f);
+		}
+		i++;
+	}
+	return (0);
 }
 /**
 *get_print_char - name function to called type dates
 * @s: parameter type char
-* Return: value of op_s the pocision 
+* Return: value of op_s the pocision
 */
 int (*get_print_char(char s))(char *z)
 {
-    i_char list[] = {
-        {'s', print_string},
-        {'c', print_char},        
-        };
-    int i = 0;
-    while (list[i].op != '\0')
-    {
-        if(list[i].op == s)
-        {
-            return ((list[i]).f);
-        }
-        i++;
-    }
-    return (0);
+	i_char list[] = {
+		{'s', print_string},
+		{'c', print_char},
+		{'%', print_percent},
+	};
+	int i = 0;
+
+	while (list[i].op != '\0')
+	{
+		if (list[i].op == s)
+		{
+			return ((list[i]).f);
+		}
+		i++;
+	}
+	return (0);
 }
