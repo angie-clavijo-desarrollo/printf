f6cdaed655ea8270c57c5f3badd8aef070db195f
lemejiamo <77980644+lemejiamo@users.noreply.github.com>
2021-03-16 01:11:43 -0500

    Funtion select

--- get_print_func.c
+++ get_print_func.c
@@ -1,27 +1,45 @@
 #include "holberton.h"
-#include "func.h"
 /**
- *get_print_func - name function to called type dates
- * @s: parameter type char
- * Return: value of op_s the pocision
- */
-int (*get_print_func(char *s))(int)
+*get_print_int - name function to called type dates
+* @s: parameter type char
+* Return: value of op_s the pocision 
+*/
+int (*get_print_int(char s))(int)
 {
-	op_i op_s[] = {
-		{"s", print_string},
-		{"c", print_char},
-		{"d", print_integer},
-		{"i", print_integer},
-		{NULL, NULL};
-		int i = 0;
-	}
-		while (op_s[i]->op != NULL)
-		{
-			if (op_s[i]->op == *s)
-			{
-				return (op_s[i]->f);
-			}
-			i++
-		}
-	return (NULL);
+    i_int list[] = {       
+        {'d', print_integer},
+        {'i', print_integer},
+        };
+    int i = 0;
+    while (list[i].op != '\0')
+    {
+        if(list[i].op == s)
+        {
+            return ((list[i]).f);
+        }
+        i++;
+    }
+    return (0);
+}
+/**
+*get_print_char - name function to called type dates
+* @s: parameter type char
+* Return: value of op_s the pocision 
+*/
+int (*get_print_char(char s))(char *z)
+{
+    i_char list[] = {
+        {'s', print_string},
+        {'c', print_char},        
+        };
+    int i = 0;
+    while (list[i].op != '\0')
+    {
+        if(list[i].op == s)
+        {
+            return ((list[i]).f);
+        }
+        i++;
+    }
+    return (0);
 }
