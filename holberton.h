60875cc8e7a3cda8cca5575fc6c76633689a45ba
lemejiamo <77980644+lemejiamo@users.noreply.github.com>
2021-03-16 01:12:48 -0500

    Added get funtion int and get funtion char

--- holberton.h
+++ holberton.h
@@ -5,32 +5,51 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <limits.h>
-​
-    /*function that produce output according format*/
-    int _printf(const char *format, ...);
-​
-    /* _strlen - count spaces */
-    int _strlen(char *s);
-​
-    /*funtion to print chars */
-    int print_char(char *vc);
-​
-    /*funtion to print strings */
-    int print_string(char *vp);
-​
-    /*funtion to print numbers */
-    int print_integer(int vi);
-​
-    /**
+
+/*function that produce output according format*/
+int _printf(const char *format, ...);
+
+/* _strlen - count spaces */
+int _strlen(char *s);
+
+/*funtion to print chars */
+int print_char(char *vc);
+
+/*funtion to print strings */
+int print_string(char *vp);
+
+/*funtion to print numbers */
+int print_integer(int vi);
+
+
+/* funtion to obtain  identifier funtion */
+int (*get_print_int(char))(int);
+
+/* funtion to obtain  identifier funtion */
+int (*get_print_char(char))(char *);
+
+
+/**
  * struct_print  - Struct  to print
  * @idnetifier: The  identifier
  * @f: The function associated
  */
-    typedef struct identifier
+typedef  struct identchar
 {
-    char *op;
-    int (*f)(int);
-} op_i;
+	char op;
+	int (*f)(char *);
+} i_char;
 
-​
-#endif /*head*/
+
+/**
+ * struct_print  - Struct  to print
+ * @idnetifier: The  identifier
+ * @f: The function associated
+ */
+typedef  struct identinteger
+{
+        char op;
+        int (*f)(int);
+} i_int;
+
+#endif
