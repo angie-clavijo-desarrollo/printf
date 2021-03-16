51326079c7f3f48816e20f4ae1026d59e59103fb
AnguiDev <angiedesarrollo@gmail.com>
2021-03-15 23:43:02 -0500

    modified head with arguments that function that remplace a switch cases

--- holberton.h
+++ holberton.h
@@ -1,7 +1,36 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+​
+    /*function that produce output according format*/
+    int _printf(const char *format, ...);
+​
+    /* _strlen - count spaces */
+    int _strlen(char *s);
+​
+    /*funtion to print chars */
+    int print_char(char *vc);
+​
+    /*funtion to print strings */
+    int print_string(char *vp);
+​
+    /*funtion to print numbers */
+    int print_integer(int vi);
+​
+    /**
+ * struct_print  - Struct  to print
+ * @idnetifier: The  identifier
+ * @f: The function associated
+ */
+    typedef struct identifier
+{
+    char *op;
+    int (*f)(int);
+} op_i;
 
-/*function that produce output according format*/
-int _printf(const char *format, ...);
-
-#endif /*head HOLBERTON.H*/
+​
+#endif /*head*/
