58b33dc17d292454ce1339c26e5fde2d2b4ff937
Edheramirez <edher.ramirez@outlook.es>
2021-03-16 23:16:20 -0500

    concatenate duplicate lines

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * _printf - simple funtion to write chars in stdout
  * @format: string to write
@@ -7,41 +8,46 @@
 int _printf(const char *format, ...)
 {
 	int cprinted = 0,  i = 0;
-	char *vc = NULL;
-
+	char c, *vc = NULL;
+	​
 	va_list(arglist);
-	if (*format == '\0') /*se valida el string de entrada */
-		return (-1);
-		va_start(arglist, format);
+	if (*format == '\0')
+		return (1);
+	va_start(arglist, format);
+	​
 	for ( ; format[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
+		if (format[i] == '%')
+		{
+			++i;
+			if (format[i] == 'd' || format[i] == 'i')
+				cprinted += (get_print_int(format[i])
+					     ((va_arg(arglist, int))));
+			else if (format[i] == 's' || format[i] == 'c' ||
+				 format[i] == 'R'
+				 || format[i] == '%')
+			{
+				if (format[i] == '%')
+					cprinted += (get_print_char(format[i])
+    						     (vc));
+				else
+					cprinted += (get_print_char(format[i])
+						   ((va_arg(arglist, char *))));				
+			}
+			else
+			{
+				c = format[i - 1];
+				write(1, &c, 1);
+				cprinted++;
+				--i;
+			}
+		}
+		else
 		{
 			write(1, &format[i], 1);
 			cprinted++;
 		}
-		else
-		{
-			++i;
-			if (format[i] == 'd' || format[i] == 'i')
-					cprinted += (get_print_int(format[i])((va_arg(arglist, int))));
-			
-			else if (format[i] == 's' || format[i] == 'c' || format[i] == '%' || format[i] == 'R')
-			{
-				if (format[i] == '%')
-					cprinted += (get_print_char(format[i])(vc));
-				else
-				{
-					cprinted += (get_print_char(format[i])((va_arg(arglist, char *))));
-				}
-			}
-			else
-			{
-				--i;
-				write(1, &format[i], 1);
-			}
-		}
 	}
 	va_end(arglist);
-return (cprinted);
+	return (cprinted);
 }
